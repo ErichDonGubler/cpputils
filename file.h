6f399173610438e8447f9c9b128f4e16f79e7ab8
Erich Gubler <erichdongubler@gmail.com>
2015-04-18 16:07:39 -0600

    Added some common parsing algorithms and .gitignore

---
+++ file.h
@@ -0,0 +1,50 @@
+/**
+ * File utilities like path manipulation
+ * Authors: Erich Gubler, erichdongubler@gmail.com
+ */
+#ifndef FILE_H
+#define FILE_H
+
+#include <string>
+
+//TODO: Move to cpputils
+std::string replaceFileExtension(const std::string& filePath, const std::string& extension)
+{
+	char c;
+	for(auto i = filePath.size() - 1; i >= 0; --i)
+	{
+		c = filePath[i];
+		switch(c)
+		{
+			case '.':
+				return filePath.substr(0, i + 1) + extension;
+ 			// If we encounter a directory slash, then we obviously don't have a file with an extension on it.
+			case '\\':
+			case '/':
+				return filePath + "." + extension;
+			default:
+				break;
+		}
+	}
+	return filePath + "." + extension;
+}
+
+std::string baseFileName(const std::string& filePath)
+{
+	char c;
+	for(auto i = filePath.size() - 1; i >= 0; --i)
+	{
+		c = filePath[i];
+		switch(c)
+		{
+			case '\\':
+			case '/':
+				return filePath.substr(i + 1);
+			default:
+				break;
+		}
+	}
+	return filePath;
+}
+
+#endif
---
+++ parsing.h
@@ -0,0 +1,19 @@
+/**
+ * 
+ * Authors: Erich Gubler, erichdongubler@gmail.com
+ */
+#ifndef PARSING_H
+#define PARSING_H
+
+// Similar to C#'s "tryParse"
+template<typename T>
+bool getNumber(T& storage)
+{
+	std::string rawInput = "";
+	getline(std::cin, rawInput);
+	std::stringstream ss;
+	ss.str(rawInput);
+	return ss >> storage;
+}
+
+#endif
