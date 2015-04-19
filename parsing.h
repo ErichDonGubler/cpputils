7a2b8cb2fdd6b14967666fd4b52985647969e851
Erich Gubler <erichdongubler@gmail.com>
2015-04-18 19:45:41 -0600

    Made input stream configgable

--- parsing.h
+++ parsing.h
@@ -5,12 +5,15 @@
 #ifndef PARSING_H
 #define PARSING_H
 
+#include <iostream>
+#include <string>
+
 // Similar to C#'s "tryParse"
 template<typename T>
-bool tryParseNumber(T& storage)
+bool tryParseNumber(T& storage, std::istream& is = std::cin)
 {
 	std::string rawInput = "";
-	getline(std::cin, rawInput);
+	getline(is, rawInput);
 	std::stringstream ss;
 	ss.str(rawInput);
 	return ss >> storage;
