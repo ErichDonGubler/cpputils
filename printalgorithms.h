77d6229c98b71521eca2c52bb5f9d16331c2b48b
Erich Gubler <erichdongubler@gmail.com>
2015-04-19 20:07:07 -0600

    Filemode changes

--- printalgorithms.h
+++ printalgorithms.h
@@ -11,44 +11,44 @@ using namespace std;
 
 // Stream functor that prints random-access ranges of the form "[a, b, c]"
 // TODO: Get rid of .size() call for true random-access interface
-template<typename T>
+template<typename T, template <typename = T, typename...> class Range>
 class print_range
 {
 private:
 	static const int DYNAMIC_SIZING = -1;
-	T& t;
+	Range<T>& range;
 	long int offset = 0;
 	long int length = DYNAMIC_SIZING;
 
 public:
-	print_range(T& t) : t(t) {}
+	print_range(Range<T>& r) : range(r) {}
 	// Use this ctor if you need to manually specify length and/or offset
 	// 
-	print_range(T& t, int o, int l) : t(t), offset(o), length(l){}
+	print_range(Range<T>& r, int o, int l) : range(r), offset(o), length(l){}
 
 	friend ostream& operator<<(ostream& os, const print_range& pr)
 	{
 		os << '[';
 		if(pr.length == DYNAMIC_SIZING)
 		{
-			auto size = pr.t.size();
+			auto size = pr.range.size();
 			switch(size)
 			{
 				case 0:
 					break;
 				case 1:
-					os << pr.t[0];
+					os << pr.range[0];
 					break;
 				default:
-					copy(pr.t.begin() + pr.offset, pr.t.end() - 1, ostream_iterator<string>(os, ", "));
-					os << pr.t[size - 1];
+					copy(pr.range.begin() + pr.offset, pr.range.end() - 1, ostream_iterator<T>(os, ", "));
+					os << pr.range[size - 1];
 					break;	
 			}
 		}
 		else
 		{
-			auto begin = pr.t.begin() + pr.offset;
-			copy(begin, pr.t.begin() + pr.offset + pr.length - 1, ostream_iterator<string>(os, ", "));
+			auto begin = pr.range.begin() + pr.offset;
+			copy(begin, pr.range.begin() + pr.offset + pr.length - 1, ostream_iterator<T>(os, ", "));
 			os << *++begin;
 		}
 		return os << ']';
