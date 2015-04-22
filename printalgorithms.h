e4313b612576fa4a52a6d48cff0b2451215cf35c
Erich Gubler <erichdongubler@gmail.com>
2015-04-22 16:36:43 -0600

    Made current functionality just use ostream functors

--- printalgorithms.h
+++ printalgorithms.h
@@ -4,54 +4,98 @@
  */
 #ifndef PRINTALGORITHMS_H
 #define PRINTALGORITHMS_H
-#include <vector>
-#include <algorithm>
-#include <iterator>
+#include <iostream>
 using namespace std;
 
 // Stream functor that prints random-access ranges of the form "[a, b, c]"
-// TODO: Get rid of .size() call for true random-access interface
-template<typename T, template <typename = T, typename...> class Range>
-class print_range
+template<typename Iter>
+class RangePrinter
 {
 private:
-	static const int DYNAMIC_SIZING = -1;
-	Range<T>& range;
-	long int offset = 0;
-	long int length = DYNAMIC_SIZING;
+	Iter begin;
+	Iter end;
 
 public:
-	print_range(Range<T>& r) : range(r) {}
-	// Use this ctor if you need to manually specify length and/or offset
-	// 
-	print_range(Range<T>& r, int o, int l) : range(r), offset(o), length(l){}
+	RangePrinter(Iter b, Iter e) : begin(b), end(e) {}
 
-	friend ostream& operator<<(ostream& os, const print_range& pr)
+	friend ostream& operator<<(ostream& os, RangePrinter& rp)
 	{
 		os << '[';
-		if(pr.length == DYNAMIC_SIZING)
-		{
-			auto size = pr.range.size();
-			switch(size)
-			{
-				case 0:
-					break;
-				case 1:
-					os << pr.range[0];
-					break;
-				default:
-					copy(pr.range.begin() + pr.offset, pr.range.end() - 1, ostream_iterator<T>(os, ", "));
-					os << pr.range[size - 1];
-					break;	
-			}
-		}
-		else
-		{
-			auto begin = pr.range.begin() + pr.offset;
-			copy(begin, pr.range.begin() + pr.offset + pr.length - 1, ostream_iterator<T>(os, ", "));
-			os << *++begin;
-		}
+		if(rp.begin != rp.end)
+			os << *rp.begin++;
+		while(rp.begin != rp.end)
+			os << ", " << *rp.begin++;
 		return os << ']';
 	}
 };
+
+template<typename T>
+void print_range(T begin, T end, std::ostream& os = std::cout)
+{
+	auto rp = RangePrinter<T>(begin, end);
+	os << rp;
+}
+
+template<typename Iter, typename F>
+class TransformedRangePrinter
+{
+private:
+	Iter begin;
+	Iter end;
+	F func;
+
+public:
+	TransformedRangePrinter(Iter b, Iter e, F f) : begin(b), end(e), func(f) {}
+
+	friend ostream& operator<<(std::ostream& os, TransformedRangePrinter& trp)
+	{
+		os << '[';
+		if(trp.begin != trp.end)
+			os << trp.func(*trp.begin++);
+		while(trp.begin != trp.end)
+			os << ", " << trp.func(*trp.begin++);
+		return os << ']';
+	}
+};
+
+template<typename T, typename F>
+void print_transformed_range(T begin, T end, F f, std::ostream& os = std::cout)
+{
+	auto trp = TransformedRangePrinter<T, F>(begin, end, f);
+	os << trp;
+}
+
+template<typename M>
+class MapPrinter
+{
+private:
+	M& map;
+
+public:
+	MapPrinter(M& m) : map(m) {}
+	friend ostream& operator<<(std::ostream& os, MapPrinter& mp)
+	{
+		auto begin = mp.map.begin();
+		auto end = mp.map.end();
+		os << '{';
+		if(begin != end)
+		{
+			auto& pair = *begin++;
+			os << pair.first << ": " << pair.second;
+		}
+		while(begin != end)
+		{
+			auto& pair = *begin++;
+			os << ", " << pair.first << ": " << pair.second;
+		}
+		return os << '}';
+	}
+};
+
+template<typename Map>
+void print_map(Map m, std::ostream& os = std::cout)
+{
+	auto mp = MapPrinter<Map>(m);
+	os << mp;
+}
 #endif
